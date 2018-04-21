87dcc309bff6b1bb440bdc5e2069a441c6d0a1dc
TaiLinhDu <linh9a0910@gmail.com>
2018-04-21 16:22:35 +0200

    version2. 1.Termin
    Convection based Routing
    Attribute basedd Routing
    Passing Data to the Views
    View Models
    Razor Views
    Partial Views

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -14,9 +15,47 @@ namespace Vidly.Controllers
         //aciton Index which return action result
         {
             var movie = new Movie() {Name = "Shrek!"};
+            var customers = new List<Customer>
+            {
+                new Customer() {Name = "Customer 1"},
+                new Customer() {Name = "Customer 2"}
+            };
 
-             
-            return View(movie);
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+
+           // var viewResult = new ViewResult();
+            //viewResult.ViewData.Model;
+            return View(viewModel);
+            //return Content("Hello World");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            //return RedirectToAction("Index", "Home", new {page = 1, sortBy = "name"});
         }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id );
+        }
+
+        //movies
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+
+        [Route("movie/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
+        public ActionResult ByReleaseDate(int year,int month)
+        {
+            return Content(year + "/" + month);
+        }           
     } 
 }
